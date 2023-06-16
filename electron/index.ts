112a15ab0988a61fd25f0f8d15a74a339169a5fe
Jean-Guillaume Isabelle <jgi@jgwill.com>
2023-06-16 01:57:37 -0400

    chg:Shorten output names

--- electron/index.ts
+++ electron/index.ts
@@ -490,7 +490,7 @@ ipcMain.on(commands.UPSCAYL, async (event, payload) => {
   const fileName = parse(fullfileName).name;
   const fileExt = parse(fullfileName).ext;
 
-  const outFile =
+  var outFile =
     outputDir +
     slash +
     fileName +
@@ -500,6 +500,24 @@ ipcMain.on(commands.UPSCAYL, async (event, payload) => {
     model +
     "." +
     saveImageAs;
+
+    //@STCIssue Upscayl Command
+
+
+    //upscayl_4x or upscayl_16x
+    var fout = outFile.replace("_upscayl_4x_","__x4");
+    fout = fout.replace("_upscayl_16x_","__x16");
+    fout = fout.replace("RealESRGAN_General_x4_v3","g");
+    fout = fout.replace("realesrgan-x4plus-anime","a");
+    fout = fout.replace("realesrgan-x4plus","r");
+    fout = fout.replace("remacri","i");
+    fout = fout.replace("ultramix_balanced","b");
+    fout = fout.replace("ultrasharp","s");
+    // command[3] = fout;
+
+    logit("      SHOULD fixed fout:\n        "+fout );
+    outFile = fout;
+    //@STCGoal Should we have a short name now :)
 
   // UPSCALE
   if (fs.existsSync(outFile)) {
